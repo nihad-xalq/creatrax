da0c66b0d3dd6362b3fb78d0d7155f0cf7690cc5
Nihad <n.abbasov@xalqsigorta.az>
2024-12-17 17:50:28 +0400

    non-null related error fixed on inputtextare

--- src/components/form/InputTextareaField.tsx
+++ src/components/form/InputTextareaField.tsx
@@ -27,13 +27,13 @@ export const InputTextareaField: FC<IInputTextareaField> = ({ ...props }) => {
       ) : null}
       <textarea
         className={`w-full py-3 px-5 border border-gray-300 placeholder-gray-300 rounded-lg focus-visible:outline-none min-h-32 max-h-48 ${props.className}`}
-        {...register(props?.name!)}
+        {...register(props.name!)}
         {...props}
         id={props?.label}
       />
       <ErrorMessage
         errors={errors}
-        name={props?.name!}
+        name={props.name!}
         render={({ message }) => (
           <p className="willSimplyFadeIn text-red-500 text-sm mt-1 pl-1">
             {message}
